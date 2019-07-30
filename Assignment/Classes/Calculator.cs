ec0ae58af178eb4c8d39116257ca5f249411b916
aamir <wasi3aamir@gmail.com>
2019-07-31 01:25:58 +0600

    Added a new Calculator class with sum and Add named method

---
+++ Assignment/Classes/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Classes
+{
+    public class Calculator
+    {
+        public static int Add()
+        {
+            var x = 3;
+            var y = 5;
+            return x + y;
+        }
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        static void Main()
+        {
+            var result = Calculator.Add();
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            var sumResult = Calculator.Sum(arr);
+            System.Console.WriteLine(result);
+            System.Console.WriteLine(sumResult);
+        }
+    }
+}
