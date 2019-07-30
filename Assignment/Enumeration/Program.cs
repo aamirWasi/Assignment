61a0bbb797c45b8e6a52cb6e503a48ea10094adf
aamir <wasi3aamir@gmail.com>
2019-07-30 12:04:16 +0600

    Updated my enum topic now in verion 1.0

--- Assignment/Enumeration/Program.cs
+++ Assignment/Enumeration/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Enumeration
 {
+    public enum RegistrationStatus
+    {
+        Active,
+        Inactive,
+        Blocked
+    }
+    public enum UserType
+    {
+        Member,
+        Support,
+        Admin,
+        SuperAdmin
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public RegistrationStatus Status { get; set; }
+        public UserType Type { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var user = new User();
+            if (user.Status == RegistrationStatus.Active &&
+                user.Type == UserType.Admin)
+            {
+                Console.WriteLine("Login in successfully...");
+            }
         }
     }
 }
