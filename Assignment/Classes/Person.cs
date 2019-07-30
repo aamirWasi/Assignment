3db96e5f42c99fefc79544650486795c27e95e88
aamir <wasi3aamir@gmail.com>
2019-07-31 01:17:03 +0600

    Added Person class with firstName lastName properties and a introduce method

---
+++ Assignment/Classes/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Introduce()
+        {
+            System.Console.WriteLine($"My Name is {FirstName}{LastName}");
+        }
+        static void Main()
+        {
+            var person = new Person()
+            {
+                FirstName = "aamir",
+                LastName = "wasi"
+            };
+            person.Introduce();
+        }
+    }
+}
