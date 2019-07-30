14e33ed56aa564f6b23bff96e5ba17f9a264f3e6
aamir <wasi3aamir@gmail.com>
2019-07-30 13:40:41 +0600

    added ConditionalStatement in IFElse namespace with seperate file .

---
+++ Assignment/IFElseAssignment/ConditionalStatement.cs
@@ -0,0 +1,13 @@
+﻿namespace IFElseAssignment
+{
+    public class ConditionalStatement
+    {
+        static void Main()
+        {
+            bool isGoldCustomer = true;
+            float price = (isGoldCustomer) ? 19.90f : 30.80f;
+            System.Console.WriteLine($"price : {price.ToString("c")}");
+        }
+
+    }
+}
