d35c75cdc1ad00be420a9d6a0a22323c34d469c5
aamir <wasi3aamir@gmail.com>
2019-07-30 22:16:18 +0600

    added AnimateDirection enum class for animating new features

--- Assignment/Enumeration/Program.cs
+++ Assignment/Enumeration/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Enumeration
 {
+    public enum AnimateDirection
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,6 +22,33 @@ namespace Enumeration
                 user.Type == UserType.Admin)
             {
                 Console.WriteLine("Login in successfully...");
+            }
+        }
+        //Add a Animated Method for Animate Enum
+
+        static void Animate(AnimateDirection direction)
+        {
+            switch (direction)
+            {
+                case AnimateDirection.Right:
+                    Console.WriteLine("Animating in Right....");
+                    break;
+                case AnimateDirection.Left:
+                    Console.WriteLine("Animating in Left....");
+
+                    break;
+                case AnimateDirection.Up:
+                    Console.WriteLine("Animating in Up...");
+
+                    break;
+                case AnimateDirection.Down:
+                    Console.WriteLine("Animating in Down...");
+
+                    break;
+                default:
+                    Console.WriteLine("I don't know what kinds of animating this is!...");
+
+                    break;
             }
         }
     }
