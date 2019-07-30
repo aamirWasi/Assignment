9da2974d9b0c5255e86b0f9a4c1df3e41f2e3a87
aamir <wasi3aamir@gmail.com>
2019-07-30 15:49:42 +0600

    While iteration with break statement

---
+++ Assignment/IterationStatement/WhileStatement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace IterationStatement
+{
+    public class WhileStatement
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your name ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                Console.WriteLine($"My Name is : {input}");
+            }
+        }
+    }
+}
