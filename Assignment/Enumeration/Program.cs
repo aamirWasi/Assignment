074d5535b360a3b6a2892ff91a692f0cd87dcc9a
aamir <wasi3aamir@gmail.com>
2019-07-30 12:00:21 +0600

    Added enum in my assignment sln

--- Assignment/Enumeration/Program.cs
+++ Assignment/Enumeration/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interfaces
+namespace Enumeration
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
