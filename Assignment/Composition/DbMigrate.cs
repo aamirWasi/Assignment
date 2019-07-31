4155c57a1f098e6bddd0bb04ded833d2bc5019c0
aamir <wasi3aamir@gmail.com>
2019-08-01 03:01:13 +0600

    separated all classes

---
+++ Assignment/Composition/DbMigrate.cs
@@ -0,0 +1,16 @@
+﻿namespace Composition
+{
+    public class DbMigrate
+    {
+        private readonly Logger _logger;
+
+        public DbMigrate(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We're migrating blah blah blah...");
+        }
+    }
+}
--- Assignment/Composition/Program.cs
+++ Assignment/Composition/Program.cs
@@ -1,44 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Composition
 {
-    public class Logger
-    {
-        public void Log(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-    public class DbMigrate
-    {
-        private readonly Logger _logger;
-
-        public DbMigrate(Logger logger)
-        {
-            _logger = logger;
-        }
-        public void Migrate()
-        {
-            _logger.Log("We're migrating blah blah blah...");
-        }
-    }
-    public class Installer
-    {
-        private readonly Logger _logger;
-
-        public Installer(Logger logger)
-        {
-            _logger = logger;
-        }
-        public void Install()
-        {
-            _logger.Log("We're installing a project...");
-        }
-    }
     class Program
     {
         static void Main(string[] args)
