549ad61a0a0d69b3551413ab9b832244dab7e67a
aamir <wasi3aamir@gmail.com>
2019-07-31 11:07:45 +0600

    Added new Assignment on Constructror

---
+++ Assignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            Console.WriteLine(person.ID);
+        }
+    }
+}
