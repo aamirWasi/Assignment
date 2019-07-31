12b1af81449f85b8907300d37ebb7a4c76c63ea1
aamir <wasi3aamir@gmail.com>
2019-08-01 02:02:56 +0600

    Added a class named Two for printing all methods without private accessor

---
+++ Assignment/AccessModifierAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierAssignment
+{
+    public class Program
+    {
+        private void Test1()
+        {
+            Console.WriteLine("Private method...");
+        }
+        internal void Test2()
+        {
+            Console.WriteLine("Internal method...");
+
+        }
+        protected void Test3()
+        {
+            Console.WriteLine("protected method...");
+
+        }
+        protected internal void Test4()
+        {
+            Console.WriteLine("protected internal method...");
+
+        }
+        public void Test5()
+        {
+            Console.WriteLine("Public method...");
+        }
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Test1();
+            program.Test2();
+            program.Test3();
+            program.Test4();
+            program.Test5();
+        }
+    }
+}
---
+++ Assignment/AccessModifierAssignment/Two.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AccessModifierAssignment
+{
+    //Without private method all methods are showed
+    public class Two:Program
+    {
+        static void Main()
+        {
+            var two = new Two();
+            two.Test2();
+            two.Test3();
+            two.Test4();
+            two.Test5();
+        }
+    }
+}
