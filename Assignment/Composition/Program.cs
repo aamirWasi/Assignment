3be30d0d3247dc03cf5435bf5c9fc18e47a1c354
aamir <wasi3aamir@gmail.com>
2019-08-01 02:59:48 +0600

    Added composition in my program for loosely couple application instead using of inheritance

--- Assignment/Composition/Program.cs
+++ Assignment/Composition/Program.cs
@@ -13,10 +13,42 @@ namespace Composition
             Console.WriteLine(message);
         }
     }
+    public class DbMigrate
+    {
+        private readonly Logger _logger;
+
+        public DbMigrate(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We're migrating blah blah blah...");
+        }
+    }
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Install()
+        {
+            _logger.Log("We're installing a project...");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var logger = new Logger();
+            var dbMigrate = new DbMigrate(logger);
+            var installer = new Installer(logger);
+
+            dbMigrate.Migrate();
+            installer.Install();
         }
     }
 }
