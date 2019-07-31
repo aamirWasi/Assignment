647c0c209727aa4f492e2e4b5a25ab7fc63cd5c5
aamir <wasi3aamir@gmail.com>
2019-07-31 11:23:40 +0600

    using this and added a list of orders in customer class

--- Assignment/ConstructorAssignment/Program.cs
+++ Assignment/ConstructorAssignment/Program.cs
@@ -6,21 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConstructorAssignment
 {
+    public class Order
+    {
+
+    }
     public class Person
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public List<Order> Orders;
         public Person()
         {
-              
+            Orders = new List<Order>();
         }
         public Person(int id)
+            :this()
         {
             ID = id;
         }
         public Person(int id,string name)
+            :this(id)
         {
-            ID = id;
             Name = name;
         }
     }
