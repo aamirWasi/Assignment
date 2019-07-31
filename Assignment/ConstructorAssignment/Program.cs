b773110a5f8718e53d4f6fd149a5d4ef646b8e68
aamir <wasi3aamir@gmail.com>
2019-07-31 11:12:56 +0600

    Added 3 constructor for mapping overloading

--- Assignment/ConstructorAssignment/Program.cs
+++ Assignment/ConstructorAssignment/Program.cs
@@ -10,13 +10,27 @@ namespace ConstructorAssignment
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public Person()
+        {
+              
+        }
+        public Person(int id)
+        {
+            ID = id;
+        }
+        public Person(int id,string name)
+        {
+            ID = id;
+            Name = name;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
+            var person = new Person(1,"aamir");
             Console.WriteLine(person.ID);
+            Console.WriteLine(person.Name);
         }
     }
 }
