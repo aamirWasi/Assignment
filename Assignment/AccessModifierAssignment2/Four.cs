eb740aa51d0ba6545b353115cd737ca345e89d40
aamir <wasi3aamir@gmail.com>
2019-08-01 02:14:32 +0600

    without print internal(bcoz in different project)

---
+++ Assignment/AccessModifierAssignment2/Four.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierAssignment2
+{
+    //without print internal(bcoz in different project)
+    class Four: AccessModifierAssignment.Program
+    {
+        static void Main(string[] args)
+        {
+            var four = new Four();
+            four.Test3();
+            four.Test4();
+            four.Test5();
+        }
+    }
+}
