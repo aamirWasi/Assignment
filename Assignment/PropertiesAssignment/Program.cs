3fd32e4136a50317cd8d90475ae7f1368cda431a
aamir <wasi3aamir@gmail.com>
2019-07-31 12:22:00 +0600

    Added properties with a get accessor

---
+++ Assignment/PropertiesAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAssignment
+{
+    public class Person
+    {
+        public DateTime BirthDate { get; set; }
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                return timeSpan.Days/365;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                BirthDate = new DateTime(1996, 2, 3)
+            };
+            Console.WriteLine(person.Age);
+        }
+    }
+}
