1034b6d9498f6eb7c62b81ffa97509c52e39aab0
aamir <wasi3aamir@gmail.com>
2019-08-01 02:05:50 +0600

    Without protected method all methods are showed

---
+++ Assignment/AccessModifierAssignment/Three.cs
@@ -0,0 +1,14 @@
+﻿namespace AccessModifierAssignment
+{
+    //Without protected method all methods are showed
+    public class Three
+    {
+        static void Main()
+        {
+            var three = new Program();
+            three.Test2();
+            three.Test4();
+            three.Test5();
+        }
+    }
+}
