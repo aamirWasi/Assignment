7e9b93fc7996a161e8ef9918cb2f1502d1d3f15d
aamir <wasi3aamir@gmail.com>
2019-08-01 02:17:01 +0600

    only print public method (bcoz in different project and not child class)

---
+++ Assignment/AccessModifierAssignment2/Five.cs
@@ -0,0 +1,12 @@
+﻿namespace AccessModifierAssignment2
+{
+    //only print public method (bcoz in different project and not child class)
+    public class Five
+    {
+        static void Main()
+        {
+            var p = new AccessModifierAssignment.Program();
+            p.Test5();
+        }
+    }
+}
