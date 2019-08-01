e45fbdf23eefdead2a2c95d4682bd248311fb1ac
aamir <wasi3aamir@gmail.com>
2019-08-01 15:21:32 +0600

    Added a new workflowEngine class as a base class

---
+++ Assignment/DynamicAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicAssignment
+{
+    public class GenericDynamic
+    {
+        public void Add<T>(T a,T b)
+        {
+            //var sum = a + b;//shows error
+            //Opeartor can not be applied of Type of T and T
+            //so use dynamic that's allow datatype of the variable is identified in the runtime
+            dynamic x = a;
+            dynamic y = b;
+            var sum = x + y;
+            Console.WriteLine($"Summation is {sum}");
+        }
+    }
+    public class Generic2<T>
+    {
+        public void Add(T a, T b)
+        {
+            //var sum = a + b;//shows error
+            //Opeartor can not be applied of Type of T and T
+            //so use dynamic that's allow datatype of the variable is identified in the runtime
+            dynamic x = a;
+            dynamic y = b;
+            var sum = x + y;
+            Console.WriteLine($"Summation is {sum}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new GenericDynamic();
+            obj.Add<int>(10, 20);
+
+
+            var obj2 = new Generic2<int>();
+            obj2.Add(20, 30);
+
+        }
+    }
+}
--- Assignment/InheritanceAssignment/Program.cs
+++ Assignment/InheritanceAssignment/Program.cs
@@ -1,10 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InheritanceAssignment
 {
+    public class WorkFlowEngine
+    {
+        private List<WorkFlowEngine> _activities;
+
+        public WorkFlowEngine()
+        {
+            _activities = new List<WorkFlowEngine>();
+        }
+        public void AddActivities(WorkFlowEngine activity)
+        {
+            _activities.Add(activity);
+        }
+        public void Run()
+        {
+            foreach (var act in _activities)
+            {
+                act.Execute();
+            }
+        }
+        public virtual void Execute()
+        {
+
+        }
+    }
+    public class WebService : WorkFlowEngine
+    {
+        public override void Execute()
+        {
+            System.Console.WriteLine("Executing call for web service...");
+        }
+    }
+    public class SendMail : WorkFlowEngine
+    {
+        public override void Execute()
+        {
+            var numofMails = 0;
+            System.Console.WriteLine("How many mails do you want to send ?");
+            numofMails = int.Parse(Console.ReadLine());
+            System.Console.WriteLine($"Executing call for sending mail...{numofMails}");
+        }
+    }
+    public class UploadVideo : WorkFlowEngine
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Executing for uploading video");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -13,6 +63,14 @@ namespace InheritanceAssignment
             System.Console.WriteLine($"Height : {text.Height}");
             text.HyperLink("mypic.jpeg");
             text.Duplicate();
+
+
+            var workEngine = new WorkFlowEngine();
+            workEngine.AddActivities(new SendMail());
+            workEngine.AddActivities(new WebService());
+            workEngine.AddActivities(new UploadVideo());
+
+            workEngine.Run();
         }
     }
 }
