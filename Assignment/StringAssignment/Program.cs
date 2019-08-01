1b509827fa4988760b340aad9114fc4e3bcbbd81
aamir <wasi3aamir@gmail.com>
2019-08-01 13:18:39 +0600

    using equal operator to compare strings

---
+++ Assignment/StringAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new Person
+            {
+                FirstName = "aamir",
+                LastName = "wasi"
+            };
+            if (user.FirstName == user.LastName)
+            {
+                Console.WriteLine("Both strings are same");
+            }
+            else
+            {
+                Console.WriteLine("Both are not same");
+            }
+        }
+    }
+}
