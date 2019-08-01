205cfa5be734584de300c521b1cad9dd25eab833
aamir <wasi3aamir@gmail.com>
2019-08-01 14:37:44 +0600

    binding the method with delegate

---
+++ Assignment/AnonymousTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypes
+{
+    public delegate string GreetingDelegate(string name);
+    class Program
+    {
+        static string Greetings(string name)
+        {
+            return $"Hello {name} a very good morning...";
+        }
+        static void Main(string[] args)
+        {
+            var obj = new GreetingDelegate(Greetings);
+            Console.WriteLine(obj("aamir"));
+        }
+    }
+}
