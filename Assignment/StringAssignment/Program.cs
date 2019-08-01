ac9c8d5494d957d19edb07e78e4b577a40bba68b
aamir <wasi3aamir@gmail.com>
2019-08-01 14:02:48 +0600

    added region

--- Assignment/StringAssignment/Program.cs
+++ Assignment/StringAssignment/Program.cs
@@ -12,6 +12,8 @@ namespace StringAssignment
         {
             var something1 = "Something";
             var something2 = "Something";
+
+            #region string compare
             if (something1.Equals(something2, StringComparison.CurrentCulture))
             {
                 Console.WriteLine("Both are same");
@@ -20,6 +22,65 @@ namespace StringAssignment
             {
                 Console.WriteLine("Both are not same");
             }
+            #endregion
+
+            #region indexing and string Length.....
+            Console.WriteLine(something2[4]);
+            for (int i = 0; i < something1.Length; i++)
+            {
+                Console.Write(something1[i]);
+            }
+
+            #endregion
+
+            #region Lower&&Substring
+
+            Console.WriteLine();
+            var subStringSomething1 = something1.Substring(0, 4).ToLower();
+            Console.WriteLine(subStringSomething1);
+            #endregion
+
+            #region Upper
+            var subStringSomething2 = something2.Substring(0, 4).ToUpper();
+            Console.WriteLine(subStringSomething2);
+            #endregion
+
+
+            #region stringEmpty
+            string sampleEmptyString = string.Empty;
+            if(string.IsNullOrWhiteSpace(sampleEmptyString))
+                Console.WriteLine("string is empty...");
+            else
+                Console.WriteLine("string is not empty...");
+            #endregion
+
+            #region Trim
+            var sampleTrim = "   sampleTrim  ";
+            Console.WriteLine(sampleTrim.Trim());
+            #endregion
+
+            #region Replace
+
+            var stringReplace = "somestring";
+            Console.WriteLine(stringReplace.Replace("some","apple"));
+
+            #endregion
+
+            #region Split
+            var sampleWords = "some1,some2,some3,some4,some5";
+            var words = sampleWords.Split(',');
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+            #endregion
+
+            #region join
+
+            var stringJoin = string.Join(",", words);
+            Console.WriteLine(stringJoin);
+
+            #endregion
         }
     }
 }
