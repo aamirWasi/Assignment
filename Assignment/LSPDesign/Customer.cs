1a6c4cb2cbfd1309921f410d46987d1c72ace4e3
aamir <wasi3aamir@gmail.com>
2019-08-02 14:28:16 +0600

    Added updated version for this this in FeatureLSP branch

---
+++ Assignment/LSPDesign/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LSPDesign
+{
+    //Updated bersion in FeatureLSP1
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+    }
+}
