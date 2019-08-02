259868a5a93efac23dc3f0e2de2c590c7a29e5bb
aamir <wasi3aamir@gmail.com>
2019-08-02 21:10:06 +0600

    failed

---
+++ Assignment/DCP13Banglawash/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP13Banglawash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            double t1, t2, sumBangla=0, sumPak = 0, t3, t4, pakTotal = 0, banglaTotal = 0;
+            for (int i = 0; i < T; i++)
+            {
+                sumBangla = 0;
+                pakTotal = 0;
+                sumPak = 0;
+                banglaTotal = 0;
+                for (int j = 0; j < 2; j++)
+                {
+                    string line = Console.ReadLine();
+                    string[] arr = line.Split(' ');
+                    t1 = double.Parse(arr[0]);
+                    t2 = double.Parse(arr[1]);
+                    t3 = double.Parse(arr[2]);
+                    t4 = double.Parse(arr[3]);
+                    sumBangla = t1 + t2;
+                    sumPak = t3 + t4;
+                    Console.WriteLine(sumBangla);
+                    Console.WriteLine(sumPak);
+                }
+                pakTotal += sumPak;
+                banglaTotal += sumBangla;
+                Console.WriteLine(banglaTotal);
+                Console.WriteLine(pakTotal);
+                if (banglaTotal < pakTotal || banglaTotal == pakTotal)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("banglawash");
+                }
+            }
+            //Console.WriteLine(banglaTotal);
+            //Console.WriteLine(pakTotal);
+            //if (banglaTotal < pakTotal || banglaTotal == pakTotal)
+            //{
+            //    Console.WriteLine("Miss");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("banglawash");
+            //}
+        }
+    }
+}
