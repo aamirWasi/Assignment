f26754088e249bf0b0616462a671fa1099d6782b
aamir <wasi3aamir@gmail.com>
2019-08-02 20:00:33 +0600

    DCP196BreakSimulator accepted

---
+++ Assignment/DCP196BreakSimulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DCP196BreakSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            double v, t, de;
+            for (int i = 0; i < T; i++)
+            {
+                string line = Console.ReadLine();
+                string[] arr = line.Split(' ');
+                v = double.Parse(arr[0]);
+                t = double.Parse(arr[1]);
+                de = -(v / t);
+                Console.WriteLine($"{de:F2}");
+            }
+        }
+    }
+}
